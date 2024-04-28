c944d6aa9556d5f8a8a640246679a72fba7914ca
Nox <167952403+Tnoxious@users.noreply.github.com>
2024-04-28 19:48:56 +1000

    Update en.lua
    English wording fixed

--- locales/en.lua
+++ locales/en.lua
@@ -1,5 +1,6 @@
 --[[ ===================================================== ]]--
 --[[           MH NPC Services Script by MaDHouSe          ]]--
+--[[              EN Corrections by Tnoxious               ]]--
 --[[ ===================================================== ]]--
 
 local Translations = {
@@ -8,13 +9,13 @@ local Translations = {
         ['get_in'] = "Get In",
     },
     notify = {
-        ['only_when_badly_injured'] = "You can only use this if you are badly injured",
+        ['only_when_badly_injured'] = "You can only use this if you are badly injured.",
         ['to_much_ems_online'] = "Contact EMS players online!",
         ['cant_pay'] = "You can't pay the amount of $%{price}, you don't have enough money.",
-        ['cooldown'] = "~r~%{job}~s~ cooldown actief ~g~%{cooldownTime}~s~ seconde",
+        ['cooldown'] = "~r~%{job}~s~ cooldown active ~g~%{cooldownTime}~s~ seconds.",
         ['call_company'] = "The %{company} has been called",
         ['jail_free_time'] = 'FREE IN: ~r~%{freetime}~s~ SECONDS',
-        ['press_e_to_enter'] = "Press ~g~[E]~s~ in %{waitTime} seconde or the %{job} leaves",
+        ['press_e_to_enter'] = "Press ~g~[E]~s~ in %{waitTime} seconds or the %{job} leaves",
         ['you_are_calling'] = "You are calling the %{job}",
         ['not_the_owner'] = 'You do not own this vehicle!',
         ['not_in_vehicle'] = 'You must be in vehicle to call!',
@@ -23,7 +24,7 @@ local Translations = {
         ['npc_mechanic_dead'] = 'Driver was killed towing! Your vehicle was sent to garage..',
         ['left_vehicle'] = "you left the vehicle",
         ['player_is_moved'] = "NPC Service was canceled you left the pickup area!",		
-        ['can_not_use_services'] = "You cannot use these services if there is a player in town with this job",
+        ['can_not_use_services'] = "You cannot use these services at this time!",
     },
     menu = {
         ['title'] = "Call Services",
